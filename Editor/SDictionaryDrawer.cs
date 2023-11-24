cc931cdb93e326bcc34929979886ffad5febaf22
GameGC <32963287+GameGC@users.noreply.github.com>
2023-11-24 14:23:30 -0600

    Update SDictionaryDrawer.cs

--- Editor/SDictionaryDrawer.cs
+++ Editor/SDictionaryDrawer.cs
@@ -1,4 +1,7 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 
 namespace GameGC.Collections.Editor
@@ -16,8 +19,55 @@ namespace GameGC.Collections.Editor
             EditorGUI.PropertyField(position,target , label, true);
             if (EditorGUI.EndChangeCheck())
             {
-                ((IUnique) (property.managedReferenceValue)).ValidateUnique();
+                ((IUnique) GetProperty(property)).ValidateUnique();
             }
+        }
+
+        private static object GetProperty(SerializedProperty prop)
+        {
+            var path = prop.propertyPath.Replace(".Array.data[", "[");
+            object obj = prop.serializedObject.targetObject;
+            var elements = path.Split('.');
+            foreach (var element in elements)
+            {
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("["));
+                    var index = Convert.ToInt32(element.Substring(element.IndexOf("["))
+                        .Replace("[", "")
+                        .Replace("]", ""));
+                    obj = GetValue(obj, elementName, index);
+                }
+                else
+                {
+                    obj = GetValue(obj, element);
+                }
+            }
+
+            return obj;
+        }
+        private static object GetValue(object source, string name)
+        {
+            if (source == null) return null;
+            var type = source.GetType();
+            var f = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            if (f == null)
+            {
+                var p = type.GetProperty(name,
+                    BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                if (p == null) return null;
+                return p.GetValue(source, null);
+            }
+
+            return f.GetValue(source);
+        }
+
+        private static object GetValue(object source, string name, int index)
+        {
+            var enumerable = GetValue(source, name) as IEnumerable;
+            var enm = enumerable.GetEnumerator();
+            while (index-- >= 0) enm.MoveNext();
+            return enm.Current;
         }
     }
 }
