f97668b9b1edd11c6a4a785b7a9cc2e8707c00e8
GameGC <32963287+GameGC@users.noreply.github.com>
2023-12-31 16:48:13 -0600

    Added drawer for dictionary like list

--- Editor/SDictionaryDrawer.cs
+++ Editor/SDictionaryDrawer.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Reflection;
 using UnityEditor;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace GameGC.Collections.Editor
 {
--- Editor/SNullableDrawer.cs
+++ Editor/SNullableDrawer.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 namespace GameGC.Collections.Editor
